665c1a1319589eaaa2c0695475584af223316031
94810 <pablo.bourdelas@gmail.com>
2016-01-14 00:03:19 +0100

    Ajout fonction d'alloc

--- logic.c
+++ logic.c
@@ -1,5 +1,14 @@
-/*
-
+/*      ____________________________________________________
+       |_|               == Logic Code ==                 |_|
+       |    You can find here all the source code of the    |
+       |          logical part of Cruxxade-2600.            |
+       |                                                    |
+       |_                  <=========>                     _|
+       |_|________________________________________________|_|
+                                                       \   ,__,
+                                                        \  (oo)____
+                                                           (__)    )\
+                                                              ||--|| *
 */
 #include "logic.h"
 
@@ -37,5 +46,46 @@ move ValidMove(vect start, vect end, hexa** board, int boardSize){
 	return Res;
 }
 
-void main(){	
+
+int AllocBoard(int boardSize, hexa*** board){
+	int i=0;
+	int res=0;	
+	
+	*board = (hexa**) malloc(boardSize*sizeof(hexa*));
+	
+	if(*board!=NULL){
+	
+		for(i=0;i<boardSize;i++){
+			(*board)[i]= (hexa*) malloc(boardSize*sizeof(hexa));
+			if((*board)[i]==NULL)
+			{
+				res=-2;
+				i=boardSize;
+			}
+		}
+	}
+	else
+		res=-1;
+
+	return res;						
 }
+
+void FreeBoard(int boardSize, hexa*** board){
+	int i=0;
+
+	for(i=0;i<boardSize;i++){
+		free((*board)[i]);	
+	}
+	
+	free(*board);
+	*board=NULL;
+}
+
+void main()
+{	
+	hexa** board;
+	
+	AllocBoard(5, &board);
+	FreeBoard(5, &board);
+}
+
