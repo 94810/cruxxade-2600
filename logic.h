f093d28c343e3992283fbea7c9fb036f5ef37f44
94810 <pablo.bourdelas@gmail.com>
2016-01-14 00:04:59 +0100

    Ajout fonction d'alloc

--- logic.h
+++ logic.h
@@ -1,6 +1,14 @@
-/*
-
-	
+/*      ____________________________________________________
+       |_|              == Logic Header ==                |_|
+       |  You can find here all data struture and function  |
+       |   prototype of the logical part of Cruxxade-2600.  |
+       |                                                    |
+       |_                  <=========>                     _|
+       |_|________________________________________________|_|
+						       \   ,__,
+						        \  (oo)____
+           						   (__)    )\
+              					              ||--|| *
 */
 
 #ifndef LOGIC
@@ -9,6 +17,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//Début Structure de Donnée
 
 typedef enum{
 	UP, DOWN, R_UP, L_DOWN, R_DOWN, L_UP
@@ -22,7 +32,6 @@ typedef enum{
 	EMPTY, PLAYER_1, PLAYER_2, CLOSED
 } case_value;
 
-
 typedef struct{
 	int x;
 	int y;
@@ -33,4 +42,11 @@ typedef struct hexa{
 	struct hexa* neighbords[6];
 } hexa;
 
+//Fin Strucutre de Donnée 
+
+//Début Prototype
+move ValidMove(vect start, vect end, hexa** board, int boardSize);
+int AllocBoard(int boardSize, hexa*** board); //return 0 if sucess ; Neg value otherwise
+void FreeBoard(int boardSize, hexa*** board);
+//Fin Prototype
 #endif
